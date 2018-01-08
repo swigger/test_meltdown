5a6a0f790103afe819d5c30fb101adacf25da9c2
xungeng <xungeng@gmail.com>
2018-01-08 15:43:40 +0800

    Initial Commit

---
+++ test_meltdown/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test_meltdown
+//
+//  Created by xungeng on 2018/1/8.
+//  Copyright © 2018年 xungeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
